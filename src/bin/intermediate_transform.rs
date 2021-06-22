484c9780c2af7261bdc160a3041dbb29ef1ffe04
Michael Straka <mstraka100@gmail.com>
2021-06-22 10:21:58 -0700

    intermediate phase cleaned up

--- src/bin/intermediate_transform.rs
+++ src/bin/intermediate_transform.rs
@@ -3,18 +3,12 @@ use anyhow::Result;
 use gumdrop::Options;
 #[allow(unused_imports)]
 use phase1_cli::*;
-use setup_utils::converters::{batch_exp_mode_from_str, subgroup_check_mode_from_str};
-use setup_utils::{
-    BatchExpMode,
-    SubgroupCheckMode,
-};
 use snark_setup_operator::transcript_data_structs::Transcript;
 use snark_setup_operator::{
     error::VerifyTranscriptError,
     utils::{
         create_full_parameters,
         remove_file_if_exists,
-        BEACON_HASH_LENGTH,
     },
 };
 use std::{
@@ -30,47 +24,14 @@ pub struct IntermediateTransformOpts {
     help: bool,
     #[options(help = "the path of the transcript json file", default = "transcript")]
     pub transcript_path: String,
-    #[options(help = "apply beacon")]
-    pub apply_beacon: bool,
-    #[options(help = "the beacon hash")]
-    pub beacon_hash: String,
-    #[options(
-        help = "whether to always check whether incoming challenges are in correct subgroup and non-zero",
-        default = "false"
-    )]
-    pub force_correctness_checks: bool,
-    #[options(
-        help = "which batch exponentiation version to use",
-        default = "auto",
-        parse(try_from_str = "batch_exp_mode_from_str")
-    )]
-    pub batch_exp_mode: BatchExpMode,
-    #[options(
-        help = "which subgroup check version to use",
-        default = "auto",
-        parse(try_from_str = "subgroup_check_mode_from_str")
-    )]
-    pub subgroup_check_mode: SubgroupCheckMode,
-    #[options(help = "whether to skip ratio check", default = "false")]
-    pub skip_ratio_check: bool,
     #[options(help = "curve", default = "bw6")]
     pub curve: String,
-
-    #[options(help = "size of chunks used")]
-    pub chunk_size: Option<usize>,
-
     #[options(help = "number powers used")]
     pub num_powers: usize,
 }
 
 pub struct IntermediateTransform {
     pub transcript: Transcript,
-    pub apply_beacon: bool,
-    pub beacon_hash: Vec<u8>,
-    pub force_correctness_checks: bool,
-    pub batch_exp_mode: BatchExpMode,
-    pub subgroup_check_mode: SubgroupCheckMode,
-    pub ratio_check: bool,
     pub num_powers: usize,
 }
 
@@ -83,33 +44,8 @@ impl IntermediateTransform {
             .expect("Should have read transcript file.");
         let transcript: Transcript = serde_json::from_str::<Transcript>(&transcript)?;
 
-        let beacon_hash = hex::decode(&opts.beacon_hash)?;
-        if beacon_hash.len() != BEACON_HASH_LENGTH {
-            return Err(
-                VerifyTranscriptError::BeaconHashWrongLengthError(beacon_hash.len()).into(),
-            );
-        }
-        let beacon_value = hex::decode(
-            &transcript
-                .beacon_hash
-                .as_ref()
-                .expect("Beacon value should have been something"),
-        )?;
-        if beacon_hash.clone() != beacon_value {
-            return Err(VerifyTranscriptError::BeaconHashWasDifferentError(
-                hex::encode(&beacon_value),
-                hex::encode(&beacon_hash),
-            )
-            .into());
-        }
         let parameters = Self {
             transcript,
-            beacon_hash,
-            apply_beacon: opts.apply_beacon,
-            force_correctness_checks: opts.force_correctness_checks,
-            batch_exp_mode: opts.batch_exp_mode,
-            subgroup_check_mode: opts.subgroup_check_mode,
-            ratio_check: !opts.skip_ratio_check,
             num_powers: opts.num_powers,
         };
         Ok(parameters)
